841a802aaac4da4a0c6c5db5dee0d56ba449bd3f
homebd <118789343+homebd@users.noreply.github.com>
2026-02-21 00:58:01 +0900

    UI와 Game을 위한 Phase 기반 이벤트 작업

---
+++ Assets/Scripts/Common/Enum.cs
@@ -0,0 +1,13 @@
+namespace Game.Enum
+{
+    public enum Phase
+    {
+        InLobby,
+        TravelSelection,
+        Travel,
+        Vote,
+        Feed,    
+        CheckWorldTree,
+        GameResult,
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,10 @@
+public class Player
+{
+    public readonly int ID;
+
+    public Player(int sessionId)
+    {
+        ID = sessionId;
+
+    }
+}
